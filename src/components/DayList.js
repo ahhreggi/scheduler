2f8df87d46d943ef45fef6054ecec7cee8e981a0
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-04-12 19:01:50 -0400

    Add components to Application.js

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "components/DayListItem.scss";
-import classNames from "classnames/bind";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
   const days = props.days.map(day => {
     return (
       <DayListItem
+        key={day.id}
         name={day.name}
         spots={day.spots}
         selected={day.name === props.day}
