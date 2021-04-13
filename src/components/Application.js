e4c12acf6176f6e7787e493c85819cd2df8563d8
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-04-12 20:57:28 -0400

    Add InterviewerList

--- src/components/Application.js
+++ src/components/Application.js
@@ -23,6 +23,7 @@ const days = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  // const [name, setInterviewer] = useState("Sylvia Palmer");
 
   return (
     <main className="layout">
