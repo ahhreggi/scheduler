89067a1a778e7775d7b152da5f0c3d19fece6e67
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-04-13 20:17:13 -0400

    Add Header

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,7 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
+import Header from "components/Appointment/Header";
 
 // =================================================================
 // Button
@@ -159,3 +160,4 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
