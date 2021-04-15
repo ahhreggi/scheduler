338ed2da1c322e670467752845de8f71c65068e7
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-04-15 13:35:40 -0400

    Add tests, transition() and back()

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,13 +2,25 @@ import { useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  function transition(newMode) {
-    setMode(newMode)
+  function transition(newMode, replace = false) {
+    setHistory(prev => {
+      // If replace is true, replace the current mode in history
+      // Otherwise, add it normally as a new mode in history
+      if (replace) {
+        prev = prev.slice(0, prev.length - 1);
+      }
+      return [...prev, newMode];
+    })
+    setMode(newMode);
   }
 
   function back() {
-
+    if (history.length === 1) return;
+    const newHistory = history.slice(0, history.length - 1);
+    setHistory([...newHistory]);
+    setMode(history[history.length - 2]);
   }
 
   return { mode, transition, back }
