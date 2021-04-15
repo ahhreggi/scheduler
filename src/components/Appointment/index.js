161013ee2145310bd0a2a0530188bd2521831131
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-04-15 13:46:59 -0400

    Update Appointment to use custom hooks

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,14 +7,35 @@ import Form from "./Form";
 import Status from "./Status";
 import Confirm from "./Confirm";
 import Error from "./Error";
+import useVisualMode from "../../hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {!props.interview && <Empty />}
-      {props.interview && <Show student={props.interview.student} interviewer={props.interview.interviewer} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+        interviewers={props.interviewers}
+        onCancel={"onCancel"}
+        onSave={"onSave"}
+        />
+      )}
     </article>
   )
 
