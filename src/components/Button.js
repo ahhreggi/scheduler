b3c30164aaab14f837318cb3582b38cb5a6864d1
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-04-19 16:20:02 -0400

    Add prop types to Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "components/Button.scss";
 import classNames from "classnames/bind";
+import PropTypes from "prop-types";
 
 export default function Button(props) {
+
+  Button.propTypes = {
+    confirm: PropTypes.bool.isRequired,
+    danger: PropTypes.bool.isRequired,
+    onClick: PropTypes.func.isRequired,
+    disabled: PropTypes.bool.isRequired,
+    children: PropTypes.string.isRequired
+  };
 
   let buttonClass = classNames("button", {
     "button--confirm": props.confirm,
